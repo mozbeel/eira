e5c089a9128e5e4c8b1574e23e1e33fc105449ce
mozbeel <matthiasleeb4@gmail.com>
2026-08-07 15:15:07 +0200

    Added basic testing

--- build.zig
+++ build.zig
@@ -94,4 +94,24 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(luac);
+
+    const lua_run_exe = b.addRunArtifact(lua);
+    const lua_run_step = b.step("run_lua", "Run the application");
+    lua_run_step.dependOn(&lua_run_exe.step);
+
+    const luac_run_exe = b.addRunArtifact(luac);
+    const luac_run_step = b.step("run_luac", "Run the application");
+    luac_run_step.dependOn(&luac_run_exe.step);
+
+    const test_step = b.step("test", "Run unit tests");
+
+    const unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("tests/tests.zig"),
+            .target = target,
+        }),
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 }
---
+++ tests/tests.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const io = std.testing.io;
+const alloc = std.testing.allocator;
+const RunResult = std.process.RunResult;
+
+fn run_lua(file: []const u8) !RunResult {
+    const result = try std.process.run(alloc, io, .{
+        .argv = &.{
+            "zig-out/bin/lua",
+            file,
+        },
+    });
+
+    std.debug.print("Output (\"{s}\"):\n{s}\n", .{ file, result.stdout });
+
+    return result;
+}
+
+fn free_lua(result: RunResult) void {
+    alloc.free(result.stderr);
+    alloc.free(result.stdout);
+}
+
+test "Hello World" {
+    const result = try run_lua("tests/hello_world.lua");
+
+    defer free_lua(result);
+}
